cd41be0d171e3ec14f31790b8643eb1f074ea691
csslick <digitone@naver.com>
2020-08-24 22:37:43 +0900

    add api urls

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ function App() {
   const [movie, setmovie] = useState([])
 
   // fetch movie - 함수는 마운트 될때 실행됨
+  const api_key = '224502cedb2aea2828098f3724fd0b0c';
   const url = 'https://api.themoviedb.org/3/movie/550?api_key=224502cedb2aea2828098f3724fd0b0c';
   const movieList = 'https://api.themoviedb.org/4/list/3?page=1&api_key=224502cedb2aea2828098f3724fd0b0c'
   const imgUrl = 'https://image.tmdb.org/t/p/w300'
+  const searchUrl = 'https://api.themoviedb.org/3/search/movie?query=starwars&api_key=224502cedb2aea2828098f3724fd0b0c'
   const url2 = 'http://hn.algolia.com/api/v1/search?query=react'
+  const trandUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`
 
   const fetchmovie = () => {
-    fetch(movieList)
+    fetch(trandUrl)
       .then(res => res.json())
       .then(data => {
         console.log(data.results)
@@ -20,7 +23,7 @@ function App() {
       })
       .catch(err => console.log(err))
   }
-
+ 
   /* 
     []파라미터: 업데이트 필요 state만 명시 빈값으로 두면 업데이트 반영안함
     []미정의시 모든 상태는 기본 업데이트 됨
