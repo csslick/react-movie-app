077273fe433d8b57dea55d911fb7f89e97391c95
csslick <digitone@naver.com>
2020-08-26 22:11:41 +0900

    search form-use effect

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ function App() {
 
   // state
   const [movie, setmovie] = useState([])
+  const [searchQuery, setSearchQuery] = useState('star wars')
 
   // fetch movie - 함수는 마운트 될때 실행됨
   const api_key = '224502cedb2aea2828098f3724fd0b0c';
   const url = 'https://api.themoviedb.org/3/movie/550?api_key=224502cedb2aea2828098f3724fd0b0c';
   const movieList = 'https://api.themoviedb.org/4/list/3?page=1&api_key=224502cedb2aea2828098f3724fd0b0c'
   const imgUrl = 'https://image.tmdb.org/t/p/w300'
-  const searchUrl = 'https://api.themoviedb.org/3/search/movie?query=starwars&api_key=224502cedb2aea2828098f3724fd0b0c'
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=224502cedb2aea2828098f3724fd0b0c`
   const url2 = 'http://hn.algolia.com/api/v1/search?query=react'
   const trandUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`
 
-  const fetchmovie = () => {
-    fetch(trandUrl)
+  const fetchMovie = () => {
+    fetch(searchUrl)
       .then(res => res.json())
       .then(data => {
         console.log(data.results)
@@ -29,13 +30,27 @@ function App() {
     []미정의시 모든 상태는 기본 업데이트 됨
   */
   useEffect(() => {
-    fetchmovie();
+    fetchMovie();
 
   }, []) 
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchMovie();
+  }
 
   return (
     <>
       <h1>movie</h1>
+      <h2>Movies</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text"  onChange={handleChange} />
+        <button>Search</button>
+      </form>
       {
         movie.map((item, i) => {
           return(
